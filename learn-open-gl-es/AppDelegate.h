a2a29a373617700c7225316b728c4b1f7498291b
Limit Liu <limit.liu@normcore.com>
2018-10-10 14:43:55 +0800

    opengles first

---
+++ learn-open-gl-es/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learn-open-gl-es
+//
+//  Created by Limit Liu on 2018/10/8.
+//  Copyright © 2018 limit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ learn-open-gl-es/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  learn-open-gl-es
+//
+//  Created by Limit Liu on 2018/10/8.
+//  Copyright © 2018 limit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
