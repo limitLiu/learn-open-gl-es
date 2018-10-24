7e54fbec3d4e109bb049ab773426311652bf8564
Limit Liu <limit.liu@normcore.com>
2018-10-24 18:14:15 +0800

    clean code

--- learn-open-gl-es/Classes/ViewController/ViewController.m
+++ learn-open-gl-es/Classes/ViewController/ViewController.m
@@ -10,7 +10,6 @@
 #import <OpenGLES/ES3/glext.h>
 
 #import "ShaderProcessor.h"
-#import "NSDate+func.h"
 
 @interface ViewController () <GLKViewDelegate>
 
@@ -129,7 +128,7 @@
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (GLsizei) width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (GLsizei) width, (GLsizei) height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
 
     free(data);
